f2c4e4f81605f901236ceeefc6d6d37fed83e204
FlowerGrid <FlowerGridCoding@gmail.com>
2024-10-28 22:48:01 -0500

    Gameplay logic works with ui

--- odinRockPaperScissors.js
+++ odinRockPaperScissors.js
@@ -1,11 +1,16 @@
 // Global variables: initialize scores
-const comScoreElement = document.querySelector('#com-score')
-const humanScoreElement = document.querySelector('#human-score')
-const gameGui = document.querySelector('.game-gui')
+const comScoreElement = document.querySelector('#com-score');
+const humanScoreElement = document.querySelector('#human-score');
+const gameGui = document.querySelector('.game-gui');
+const matchResultElement = document.querySelector('.match-result');
+let matchResult = '';
+if (gameGui) {
+    console.log('Game GUI exists');
+}
 let comScore = 0;
 let humanScore = 0;
-let matchResult;
-let hiScore = Math.max(humanScore, comScore);
+let hiScore = 0;
+
 
 function getComputerChoice() {
     let randNum = Math.floor(Math.random() * 100);
@@ -19,23 +24,11 @@ function getComputerChoice() {
 }
 
 
+function playRound(humanChoice) {
+    matchResultElement.textContent = '';
 
-// function getHumanChoice () {
-//     let humanChoice = '';
-//     while (
-//         humanChoice.toLowerCase() != 'rock' &&
-//         humanChoice.toLowerCase() != 'paper' &&
-//         humanChoice.toLowerCase() != 'scissors'
-//     ) {
-//         humanChoice = prompt('Please Choose: "Rock", "Paper", or "Scissors"');  
-//     }
-//     return humanChoice.toLowerCase();
-// }
-
-function playRound() {
     let comChoice = getComputerChoice();
-    let humanChoice = getHumanChoice();
-    let matchResult;
+    // let humanChoice = getHumanChoice();
     console.log('com: ' + comChoice);
     console.log('player: ' + humanChoice);
 
@@ -78,12 +71,121 @@ function playRound() {
         }
     }
 
-    switch (matchResult) {
-        case 'human':
-            humanScore += 1;
-            break;
-        case 'com':
-            comScore += 1;
-            break;
+    // Build match result message
+    const winChoiceElement = document.createElement('span');
+    winChoiceElement.classList.add('result-message');
+
+    const loseChoiceElement = document.createElement('span');
+    loseChoiceElement.classList.add('result-message');
+
+    const winnerElement = document.createElement('span');
+    winnerElement.classList.add('result-message')
+
+    if (matchResult === 'tie') {
+        winChoiceElement.textContent = 'Tie';
+        matchResultElement.appendChild(winChoiceElement);
+    } else {
+        switch (matchResult) {
+            case 'human':
+                humanScore += 1;
+                winChoiceElement.textContent = humanChoice.charAt(0).toUpperCase() + 
+                    humanChoice.slice(1);
+                loseChoiceElement.textContent = comChoice;
+                winnerElement.textContent = 'human';
+    
+                break;
+            case 'com':
+                comScore += 1;
+                winChoiceElement.textContent = comChoice.charAt(0).toUpperCase() + 
+                    comChoice.slice(1);
+                loseChoiceElement.textContent = humanChoice;
+                winnerElement.textContent = 'computer';
+                break;
+    }
+
+    matchResultElement.appendChild(winChoiceElement);
+    matchResultElement.append(' beats ');
+    matchResultElement.appendChild(loseChoiceElement);
+    matchResultElement.append(', ');
+    matchResultElement.appendChild(winnerElement);
+    matchResultElement.append(' wins!');
+    }
+
+    humanScoreElement.textContent = humanScore;
+    comScoreElement.textContent = comScore;
+
+    hiScore = Math.max(humanScore, comScore)
+    console.log(hiScore)
+
+    if (hiScore >= 5) {
+        gameGui.removeEventListener('click', clickHandler)
+        displayWinner();
     }
 }
+
+
+function displayWinner () {
+    gameGui.textContent = '';
+    let resultElement = document.createElement('div');
+    resultElement.classList.add('match-result')
+    gameGui.appendChild(resultElement);
+
+
+    const winnerElement = document.createElement('span');
+    winnerElement.classList.add('result-message');
+    const winScoreElement = document.createElement('span');
+    winScoreElement.classList.add('result-message');
+    const loseScoreElement = document.createElement('span');
+    loseScoreElement.classList.add('result-message');
+    let winner;
+    let winScore;
+    let loseScore;
+    
+    if (comScore < humanScore) {
+        winner = 'Human';
+        winScore = humanScore;
+        loseScore = comScore;
+    } else {
+        winner = 'Computer';
+        winScore = comScore;
+        loseScore = humanScore;
+    }
+    winnerElement.innerText = `${winner} wins! `;
+    winScoreElement.innerText = winScore;
+    loseScoreElement.innerText = loseScore;
+
+    resultElement.appendChild(winnerElement);
+    resultElement.appendChild(winScoreElement);
+    resultElement.append(' to ');
+    resultElement.appendChild(loseScoreElement);
+
+    let playAgainButton = document.createElement('button');
+    playAgainButton.textContent = 'Play Again?';
+    gameGui.appendChild(playAgainButton);
+    playAgainButton.addEventListener('click', () =>{
+        location.reload();
+    });
+}
+
+
+gameGui.addEventListener('click', clickHandler);
+    
+    
+function clickHandler(event) {
+    let target = event.target;
+    let humanChoice = '';
+
+    switch (target.getAttribute('data-value')) {
+        case 'rock':
+            humanChoice = 'rock';
+            break;
+        case 'paper':
+            humanChoice = 'paper';
+            break;
+        case 'scissors':
+            humanChoice = 'scissors';
+            break
+    }
+
+    playRound(humanChoice);
+};
